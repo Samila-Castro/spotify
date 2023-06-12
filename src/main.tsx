fbccbb4d24b0cdbd8500e289b45e9629d47d4523
Samila Vieira <samilav1997@gmail.com>
2023-06-12 13:59:40 -0300

    Removedo rotas

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "rsuite/dist/rsuite.min.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home.tsx";
-import Login from "./pages/Login/Login.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "home",
-    element: <Home />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Home />
   </React.StrictMode>
 );
